a6a73c63aaf6619334d9e357fbea49ead4963938
homiehe <36011867+homiehe@users.noreply.github.com>
2018-03-30 21:03:54 -0400

    Add files via upload

--- shiny_U80494546_Hongyi He.R
+++ shiny_U80494546_Hongyi He.R
@@ -3,6 +3,7 @@ library(zoo)
 library(shiny)
 library(shinydashboard)
 library(rsconnect)
+library(leaflet)
 
 #load the data
 data <- read.csv("data.csv")
@@ -18,83 +19,118 @@ data_avg <- as.tibble(data_avg)
 data_veg <- read.csv("veg_data3.csv")
 data_veg <- as.tibble(data_veg)
 data_veg <- data_veg %>% filter(label == "RESTRICTED USE CHEMICAL", Unit.of.Measurement==" MEASURED IN LB")
+tox <- read_csv("chemical_tox_shiny.csv")
+tox <- tox %>% select(-1)
+broc <- read_csv("broc.csv")
+Caul <- read_csv("Caul.csv")
 
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
+  
   dashboardHeader(title = "Basic dashboard"),
+  
   dashboardSidebar(    
     sidebarMenu(
       menuItem("Temperature", tabName = "dashboard", icon = icon("dashboard")),
       menuItem("Vegetable", tabName = "widgets", icon = icon("th"))
     )
   ),
+  
   dashboardBody(
     tabItems(
       tabItem(tabName = "dashboard",
               fluidRow(
-                box(
-                  title = "Select the range of year",
-                  status="success",
-                  solidHeader = TRUE,
-                  sliderInput("max", 
+                column(width = 12,
+#####
+                  box(
+                    title = "Select the range of year",
+                    status="success",
+                    solidHeader = TRUE,
+                    sliderInput("max", 
                               label = "Range of year:",
                               min = 1987, max = 2017, value = c(2016,2017)),
-                  hr(),
-                  radioButtons("Type", "Type of temperature:",
+                    hr(),
+                    radioButtons("Type", "Type of temperature:",
                                c("Air Temperature" = 'ATMP',
                                  "Water Temperature" = 'WTMP')),
-                  hr(),
-                  helpText("Data from the NOAA.")
-                ),
-                
-                tabBox(
-                  title = "Berling Sea Temperature",
+                    hr(),
+                    helpText("This is the air and sea temperature from 1987 to 2017 recorded by a buoy in the middle of Berling Sea. Which shows a remarkable increase.")
+                      ),
+#####
+
+#####
+                  tabBox(
+                    title = "Berling Sea Temperature",
                   
-                  id = "tabset1",
+                    id = "tabset1",
                   
-                  side = "left", 
+                    side = "left", 
                   
-                  height = "250px",
+                    height = "250px",
                   
-                  selected = "Daily air temperature",
+                    selected = "Daily air temperature",
                   
-                  tabPanel("Daily",
+                    tabPanel("Daily",
                            status="success",
                            plotOutput("graph")),
                   
-                  tabPanel("Yearly",
+                    tabPanel("Yearly",
                            status="success",
                            plotOutput("graph2")),
                   
-                  tabPanel("Regression line",
+                    tabPanel("Regression line",
                            status="success",
                            plotOutput("graph3")
                   )
                 )
+#####
+              ),
+
+                column(width = 6,
+                       box(title = "Buoy Location", status = "warning", width = NULL, 
+                           leafletOutput("buoy_map")
+                       )
               )
-      ),
+      )),
       
-      tabItem(tabName = "widgets",
-              fluidRow(
-                box(
-                  selectInput("a", "Choose a commodity:",choices = c(2006,2010,2014,2016)),
-                  selectInput("b", "Choose a commodity:",choices = c("BROCCOLI","CAULIFLOWER"))
-               ),
-                box(
-                  title = "Chemcials of vegetable compared with their toxicity",
-                  status= "success",
-                  solidHeader = TRUE,
-                  plotOutput("graph4"),              
-                  hr(),
-                  helpText("Data from EPA"),
-                  verbatimTextOutput("dateText")
-                 )
-              )
+tabItem(tabName = "widgets",
+        fluidRow(
+          box(
+            selectInput("a", "Choose a commodity:",choices = c(2006,2010,2014,2016)),
+            selectInput("b", "Choose a commodity:",choices = c("BROCCOLI","CAULIFLOWER"))
+          ),
+          tabBox(
+            tabPanel("Chemcials content of vegetable",
+                     status = "success",
+                     solidHeader = TRUE,
+                     plotOutput("graph4"),              
+                     hr(),
+                     helpText("Amount of toxic chemicals contained in vegetable. Data from EPA"),
+                     verbatimTextOutput("dateText")
+            ),
+            
+            tabPanel("Toxicity of Chemcials",
+                     status = "success",
+                     solidHeader = TRUE,
+                     plotOutput("graph5"),
+                     hr(),
+                     helpText("The LD50(the amount of the substance required to kill 50% of the test population.) level of the chemical that can be detected in vegtables. Data from EPA")
+            ),
+            
+            tabPanel("Chemicals content of vegetable compared to their toxicity",
+                     status = "warning",
+                     solidHeader = TRUE,
+                     plotOutput("graph6"),
+                     hr(),
+                     helpText("Chemicals content of vegetablecompared to their LD50 level.Data from EPA")
             )
-      )
+          )
+        )
+)
     )
   )
+)
 
 server <- function(input, output) {
   
@@ -112,6 +148,14 @@ server <- function(input, output) {
   
   d <- reactive({
     filter(data_veg, Year==input$a) %>% filter(Commodity == input$b)
+  })
+  
+  e <- reactive({
+    if(input$b=="CAULIFLOWER"){
+      filter(Caul, Year==input$a)
+    }else{
+      filter(broc, Year==input$a)
+    }
   })
   
   output$graph <- renderPlot({
@@ -162,8 +206,30 @@ server <- function(input, output) {
       labs(y = "Values(LB) ",x = "Chemical Name")
   })
   
+  output$graph5 <- renderPlot({
+    data <- as.data.frame(tox)
+    rownames(data) <- data[,1]
+    ggplot(data,mapping=aes(x = Name, y=`Values for LD50 on rats`))+
+      geom_bar(stat="identity", position="dodge",aes(fill=Name))+
+      coord_flip()+
+      labs(y = "Values(mg/kg)",x = "Chemical Name")
+  })
+  
+  output$graph6 <- renderPlot({
+    ggplot(e(), aes(x= Name, y=value )) + 
+      geom_bar(stat="identity",position = "dodge",aes(fill=Toxicity)) + 
+      labs(y = "Values(LB) ",x = "Chemical Name") +
+      coord_flip()+
+      labs(title=paste("LD50(mg/kg) and Real content(lb) of",input$b))
+  })
+  
+  output$buoy_map <- renderLeaflet({
+    m <- leaflet() %>% addTiles() %>% setView(177.738, 57.026, zoom = 4) %>% 
+      addMarkers(lng=177.738, lat=57.026, popup="The Buoy (ID:46035)") 
+    m
+  })
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
